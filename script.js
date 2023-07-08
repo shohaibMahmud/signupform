7c7f65288904affb7609ca675e2b173fcf31452e
shohaibMahmud <shohaib.mahmud@gmail.com>
2023-07-08 17:23:18 -0400

    Add validation and final styling

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelector("button").
+addEventListener("click", ()=>{
+    passwd = document.querySelector("#password");
+    confPass = document.querySelector("#confirm-pass");
+    
+    if ( passwd.value === confPass.value ){
+        if ( passwd.classList.length > 0) {
+            passwd.classList.pop();
+            document.querySelector("#err-msg").style["visibility"] = "hidden";    
+        }
+    }else{
+        document.querySelector("#err-msg").style["visibility"] = "visible";
+        passwd.classList.add("error");
+        confPass.classList.add("error");     
+    }
+});
